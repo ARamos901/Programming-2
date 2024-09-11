d46ba4807119931099012ca2d7fb336b0fd106dd
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2024-09-11 17:46:46 -0400

    Add files via upload

---
+++ Ramos_CW3.cpp
@@ -0,0 +1,29 @@
+//
+//  Class work 3
+//
+//  Aiden Ramos
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    const int MIN_NUM=1,
+    MAX_NUM=10;
+    
+    
+    int num;
+    
+    cout<<"Number Number Squared \n";
+    cout<<"-----------------------\n";
+    
+    
+    for (num=MIN_NUM;num<=MAX_NUM;num++)
+        cout<<num<<"\t\t"<<(num*num)<<endl;
+    
+    
+    
+    
+    return 0;
+}
