93b87db98e63deb5e0b8cd10575dad4629a3cacd
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2024-10-29 11:39:57 -0400

    Add files via upload

---
+++ Ramos_CW10.cpp
@@ -0,0 +1,29 @@
+//
+//  Aiden Ramos
+//  CW-10_29
+//
+//  Created by Aiden Ramos on 10/29/24.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+       
+    int x=25;
+    
+    int *ptr=nullptr;
+    
+    ptr=&x;
+    
+    cout<<"The value in x is "<<x<<endl;
+    cout<<"The address of x is "<<ptr<<endl;
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
