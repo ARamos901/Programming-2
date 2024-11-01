5614ecc4b8f206b83694cf8af9a65960134973bc
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2024-11-01 13:38:06 -0400

    Add files via upload

--- Ramos_CW10.cpp
+++ Ramos_CW10.cpp
@@ -1,27 +1,64 @@
 //
 //  Aiden Ramos
-//  CW-10_29
+//  CW-10_31
 //
-//  Created by Aiden Ramos on 10/29/24.
+//  Created by Aiden Ramos on 10/31/24.
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() {
-       
+    /*
     int x=25;
-    
     int *ptr=nullptr;
     
     ptr=&x;
     
-    cout<<"The value in x is "<<x<<endl;
-    cout<<"The address of x is "<<ptr<<endl;
-    return 0;
+    
+    cout<<"Here is the value in x,printed twice: \n";
+    cout<<x<<endl;
+    cout<<*ptr<<endl;
+    
+    *ptr=100;
     
     
+    cout<<"once again,here is the value in x: \n";
+    cout<<x<<endl;
+    cout<<*ptr<<endl;
+    */
     
+    double *sales=nullptr,total=0.0,average;
     
+    int numDays,count;
+    
+    cout<<"How many days of sales amounts do you wish ";
+    cout<<"To process? ";
+    cin>>numDays;
+    
+    sales=new double[numDays];
+    
+    cout<<"Enter the sales amounts below. \n";
+    
+    for(count=0;count<numDays;count++)
+    {
+        cout<<"Day "<<(count+1)<<": ";
+        cin>>sales[count];
+    }
+    
+    for(count=0;count<numDays;count++)
+    {
+        total+=sales[count];
+    }
+    
+    average=total/numDays;
+    
+    cout<<fixed<<showpoint<<setprecision(2);
+    cout<<"\n\nTotal Sales: $"<<total<<endl;
+    cout<<"Average Sales: $"<<average<<endl;
+    
+    delete[]sales;
+    sales=nullptr;
     
     
     
